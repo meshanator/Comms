5c3623578c5d19377fbb2b8ba3adbff750e3c78d
Meshan Naidoo <meshann@gmail.com>
2019-02-17 20:39:44 +0200

    add rudimentary code to send and receive messages on a channel

--- Receiver/Program.cs
+++ Receiver/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace Receiver
 {
 	class Program
 	{
+		public const string HostName = "localhost";
+		public const string MainQueueName = "main";
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Starting Receiver Console App");
+
+			var queueFactory = new ConnectionFactory { HostName = HostName };
+			while (true)
+			{
+				using (var queueConnection = queueFactory.CreateConnection())
+				using (var queueChannel = queueConnection.CreateModel())
+				{
+					queueChannel.QueueDeclare(MainQueueName, false, false, false, arguments: null);
+
+					var consumer = new EventingBasicConsumer(queueChannel);
+					consumer.Received += (model, ea) =>
+					{
+						var messageBody = ea.Body;
+						var messageDecoded = Encoding.UTF8.GetString(messageBody);
+						Console.WriteLine($"time: {DateTime.Now.TimeOfDay}, message: {messageDecoded}");
+					};
+
+					queueChannel.BasicConsume(MainQueueName, true, consumer);
+					Console.ReadLine();
+				}
+			}
 		}
 	}
 }
--- Sender/Program.cs
+++ Sender/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Text;
+using RabbitMQ.Client;
 
 namespace Sender
 {
 	class Program
 	{
+		public const string HostName = "localhost";
+		public const string MainQueueName = "main";
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Starting Sender Console App");
+
+			var queueFactory = new ConnectionFactory { HostName = HostName };
+			while (true)
+			{
+				using (var queueConnection = queueFactory.CreateConnection())
+				using (var queueChannel = queueConnection.CreateModel())
+				{
+					queueChannel.QueueDeclare(MainQueueName, false, false, false, null);
+
+					var messageToSend = Console.ReadLine();
+					var encodedMessage = Encoding.UTF8.GetBytes(messageToSend);
+
+					queueChannel.BasicPublish(string.Empty, MainQueueName, null, body: encodedMessage);
+					Console.WriteLine("message sent");
+				}
+			}
 		}
 	}
 }
